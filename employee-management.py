8ff7bf8632ea666d2eab445388b4f1c2c82a3816
Vish Pillai <vishakhpillai@berkeley.edu>
2018-01-10 18:50:09 -0800

    Finished the the get_groups and get_events part of the project

---
+++ employee-management.py
@@ -0,0 +1,34 @@
+import sys
+# HOME_DIR == 
+sys.path.append('/Users/khangnguyen/Desktop/Google API/Google Sheets')
+sys.path.append('/Users/khangnguyen/Desktop/Google API/Google Calendar')
+
+from quickstartCal.py
+from quickstartSheets.py
+
+SID = int(sys.argv[0])
+
+def get_groups(SID):
+  	return quickstartSheets.py.main(SID)
+
+groups = get_groups(SID)
+
+def get_events(my_groups):
+		events = quickstartCal.py.main()
+    sortedevents = []
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        try:
+    			description = event['description']
+          groups = [description]
+        	splitgroups = groups[0].split(":")
+        	for group in splitgroups:
+          		if group in my_groups:
+            			if event not in sortedevents:	
+                			sortedevents.append(event)
+        except KeyError:
+          None
+    return sortedevents
+        
