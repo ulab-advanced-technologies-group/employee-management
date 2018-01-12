d55e6921a1d685437868aa16347200f361d68aca
Vish Pillai <vishakhpillai@berkeley.edu>
2018-01-11 19:41:07 -0800

    Changes made to Group class

--- Group.py
+++ Group.py
@@ -2,6 +2,33 @@ class Group :
     def __init__(self, name) :
         self.name = name
         self.subgroups = []
+        self.people = []
 
     def addsubGroup(self, obj) :
         self.subgroups.append(obj)
+
+    def createSheet(self) :
+        import sys
+        SID = int(sys.argv[1])
+
+        from GoogleCalendar import quickstartCal
+        from GoogleSheets import quickstartSheets
+
+    def removeSheet(self) :
+        import sys
+        SID = int(sys.argv[1])
+
+        from GoogleCalendar import quickstartCal
+        from GoogleSheets import quickstartSheets
+
+        def addPerson(Person) :
+            if Person in self.people :
+                print("This person already exists in the group.")
+            else :
+                self.people.append(Person)
+
+        def removePerson(Person) :
+            if Person not in self.people :
+                print("This person does not exist in the group.")
+            else
+                self.people.remove(Person)
