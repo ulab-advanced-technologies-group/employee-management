1370d56d4fcbb8de26f19f9dee5485a0a005918b
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro.local>
2018-01-14 18:30:18 -0800

    added remove fxns

--- GoogleSheets/quickstartSheets.py
+++ GoogleSheets/quickstartSheets.py
@@ -85,7 +85,7 @@ def main(SID):
             pass # Ignores sheets that do not have SID as a column
     return sortedgroups
 
-def sheets():
+def remove_from_all(SID):
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
@@ -96,21 +96,62 @@ def sheets():
     spreadsheetId = '1k5OgXFL_o99gbgqD_MJt6LuggL4KRGBI27SIW45-FgQ'
     sheet_metadata = service.spreadsheets().get(spreadsheetId='1k5OgXFL_o99gbgqD_MJt6LuggL4KRGBI27SIW45-FgQ').execute()
     sheets = sheet_metadata.get('sheets', '')
+    groups = []
+    for sheet in sheets:
+        title = sheet.get("properties", {}).get("title", "Sheet1")
+        groups.append(title)
 
+    for group in groups:
+        remove_from_group(SID, group)
+
+def remove_from_group(SID, group):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1k5OgXFL_o99gbgqD_MJt6LuggL4KRGBI27SIW45-FgQ'
+    sheet_metadata = service.spreadsheets().get(spreadsheetId='1k5OgXFL_o99gbgqD_MJt6LuggL4KRGBI27SIW45-FgQ').execute()
+    sheets = sheet_metadata.get('sheets', '')
+    groups = []
+    sheetIds = []
     requests = []
-    # Change the spreadsheet's title.
-    requests.append({
-        'updateSpreadsheetProperties': {
-            'properties': {'title': 'Roster2'},
-            'fields': 'title'
-        }
-    })
+    for sheet in sheets:
+        title = sheet.get("properties", {}).get("title", "Sheet1")
+        sheetId = sheet.get("properties", {}).get("sheetId")
+        groups.append(title)
+        sheetIds.append(sheetId)
+    sheetorder = 0
 
-    body = {'requests': requests}
-    response = service.spreadsheets().batchUpdate(spreadsheetId='1k5OgXFL_o99gbgqD_MJt6LuggL4KRGBI27SIW45-FgQ', body=body).execute()
-    find_replace_response = response.get('replies')[1].get('findReplace')
-    print('{0} replacements made.'.format(
-        find_replace_response.get('occurrencesChanged')))
+    for rangeName in groups:
+        if group == rangeName:
+            roworder = 0
+            result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
+            values = result.get('values', [])
+            try:
+                SIDindex = values[0].index("SID")
+                for row in values:
+                    if str(SID) == row[SIDindex]:
+                        requests.append({
+                            'deleteDimension': {
+                                'range': {
+                                    'sheetId': sheetIds[sheetorder],
+                                    'dimension': 'ROWS',
+                                    'startIndex': roworder,
+                                    'endIndex': roworder + 1,
+                                }
+                            }
+                        }
+                    )
+                    roworder += 1
+            except:
+                pass # Ignores sheets that do not have SID as a column
+        sheetorder += 1
+    if requests:
+        body = {'requests': requests}
+        response = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body=body).execute()
 
 if __name__ == '__main__':
     main()
