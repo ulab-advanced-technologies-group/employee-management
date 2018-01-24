f4c299bf0d0150d65a3232aff08a308e7b0f5a16
Cibi Pari <cibipari@gmail.com>
2018-01-24 13:30:11 -0800

    Made some initial modifications to the Group and Person file.

--- Person.py
+++ Person.py
@@ -1,23 +1,34 @@
+"""
+After making modifications to a person, use the save() function to commit the changes
+to the spreadsheet.
+"""
+
 class Person:
-    def __init__(self, FirstName, MiddleName, LastName, SID, Email, PhoneNumber) :
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.Roles = []
+    def __init__(self, first_name='', middle_name='', last_name='', SID='', email='', phone='', majors=[], year='',
+                        back_id='', dietary_preferences='', expected_graduation='', classes=[],
+                        goals='', accounts=[], accesses=[]):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
         self.SID = SID
-        self.MiddleName = MiddleName
-        self.Email = Email
-        self.PhoneNumber = PhoneNumber
-        self.DietaryPreferences = None
-        self.schedule = None
-        self.Groups = []
+        self.email = email
+        self.phone = phone
+        self.majors = majors
+        self.year = year
+        self.back_id = back_id
+        self.dietary_preferences = dietary_preferences
+        self.expected_graduation = expected_graduation
+        self.classes = classes
+        self.goals = goals
+        self.accounts = accounts
+        self.accesses = accesses
+        # Stores the string names of the groups that this person is a part of. Using the get_group() in employee-management.py
+        # to retrieve the Group object for this group. Since order does not matter, we will use a set.
+        self.groups = set()
 
-    def __init__(self, FirstName, LastName, SID) :
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.SID = SID
-
-    def addGroup(Group) :
-        self.Groups.append(Group)
+    # Adds the string name of a group to this person.
+    def add_group(self, group) :
+        self.groups.append(group)
 
     def addRole(Role) :
         self.Roles.append(Role)
