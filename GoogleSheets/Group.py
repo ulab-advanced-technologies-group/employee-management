d4ce36fbaae561699e7170529b91e47e7be06358
Cibi Pari <cibipari@gmail.com>
2018-01-25 09:58:21 -0800

    Get_group and get_person skeleton added.

--- GoogleSheets/Group.py
+++ GoogleSheets/Group.py
@@ -7,6 +7,7 @@ that string name.
 After making modifications to a group, use the save() function to commit the changes
 to the spreadsheet.
 """
+from employee-management import get_group
 
 class Group:
 
@@ -26,22 +27,23 @@ class Group:
 
     # Takes in a string name of the subgroup we are adding as a child of this group.
     def add_subgroup(self, subgroup):
-        if isinstance(subgroup, Group)
+        if type(subgroup) == str:
             self.subgroups.append(subgroup)
 
     # Adds a person to the group if they are not already in it. People can only be added to groups
     # that are leaves in the ULAB organization. Traverse the path back to the root and add the person 
-    # to the group's respective column on mainroster. 
+    # to each ancestor group's respective column on mainroster. Returns true if the person is added successfully. 
     def add_person_to_group(self, person, role):
         if not person or not isinstance(person, Person):
             print("Please provide a proper person.")
-            return
+            return False
         if not self.isLeaf():
             print("Please specify a more specific subgroup to add this person to.")
-            return
+            return False
 
         if person.SID in self.people:
             print("This person already exists in the group.")
+            return True
         else :
             self.people[person.SID] = role
             group = self
@@ -49,6 +51,7 @@ class Group:
                 if group.name not in person.groups:
                     person.groups.add(group.name)
                 group = group.parent
+            return True
 
     # Removes a person from this group if they are in the group. If this group is a leaf, the person is only removed from this leaf group.
     # If this group is an internal node, we remove the person from all the subgroups as well. Returns true if the remove is successful and
@@ -56,16 +59,19 @@ class Group:
     def remove_person_from_group(self, person):
         if not person or not isinstance(person, Person):
             print("Please provide a proper person.")
-            return
+            return False
 
         if self.isLeaf():
             if person.SID not in self.people:
                 print("This person does not exist in the group.")
+                return True
             else
                 del self.people[person.SID]
         else:
+            # Remove this group from the set of group names for the person.
+            person.groups.remove(self.name)
+            # Recursively remove the person from subgroups. 
             for subgroup_name in self.subgroups:
                 subgroup = Group.get_group(subgroup_name)
-                if isinstance(subgroup, Group):
-
-    def 
+                if subgroup and isinstance(subgroup, Group):
+                    subgroup.remove_person_from_group(person)
